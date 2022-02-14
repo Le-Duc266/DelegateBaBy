14c46ea76dc83d632c7b42bf95f639fe415543a0
duc <letrungduc15082001@gmail.com>
2022-02-14 14:09:35 +0700

    first commit

---
+++ BirthdayBaBy/DelegateBirthdayBaBy.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayBaBy
+{
+    class DelegateBirthdayBaBy
+    {
+		public delegate void EnventBirthdayBaBy(string birthday, string listPhoneNumber);
+
+		public class BaBy
+		{
+			public event EnventBirthdayBaBy BirthdayBaBy;
+			public string[] listPhone;
+			public string birthday;
+
+			public BaBy()
+			{
+				listPhone = new string[0];
+			}
+
+			public string Birthday
+			{
+				get
+				{
+					return birthday;
+				}
+				set
+				{
+					birthday = value;
+					if (BirthdayBaBy != null && listPhone.Length > 0)
+					{
+						foreach (var item in listPhone)
+						{
+							BirthdayBaBy(value, item);
+						}
+					}
+				}
+			}
+			public void Guests(string guests)
+			{
+				if (listPhone.Length == 0)
+				{
+					listPhone = new string[1];
+					listPhone[0] = guests;
+				}
+				else
+				{
+					string[] listPhoneNew = new string[listPhone.Length + 1];
+					for (int i = 0; i < listPhone.Length; i++)
+					{
+						listPhoneNew[i] = listPhone[i];
+					}
+					listPhoneNew[listPhone.Length] = guests;
+					listPhone = listPhoneNew;
+				}
+			}
+
+		}
+	}
+}
---
+++ BirthdayBaBy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BirthdayBaBy.DelegateBirthdayBaBy;
+
+namespace BirthdayBaBy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaBy bb = new BaBy();
+            bb.Guests("01");
+            bb.Guests("12");
+            bb.Guests("23");
+            bb.Guests("34");
+            bb.BirthdayBaBy += ShowMess;
+            bb.Birthday = "01/01/2022";
+        }
+        public static void ShowMess(string birthday, string phoneNumber)
+        {
+            Console.WriteLine("Em be da chao doi vao ngay: {0} - Moi ban {1} den chung vui voi gia dinh chung toi", birthday.ToString(), phoneNumber);
+        }
+    }
+}
